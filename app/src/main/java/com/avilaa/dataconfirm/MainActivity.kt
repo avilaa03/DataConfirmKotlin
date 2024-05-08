76c5aeb20634cf86d682bdda0d1069f1e61637d9
avilaa03 <lucasdeavila2003@gmail.com>
2024-05-07 22:19:13 -0300

    DataConfirm FIXED

--- app/src/main/java/com/avilaa/dataconfirm/MainActivity.kt
+++ app/src/main/java/com/avilaa/dataconfirm/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.avilaa.dataconfirm
 
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -23,13 +24,16 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -39,7 +43,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DataConfirmTheme {
-                val navController = rememberNavController() // Cria o NavController
+                val navController = rememberNavController()
+                val viewModel: DataViewModel = viewModel()
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues->
                     NavHost(
@@ -47,16 +52,26 @@ class MainActivity : ComponentActivity() {
                         startDestination = "data_confirm_screen",
                         modifier = Modifier.padding(paddingValues)
                     ) {
+
                         composable("data_confirm_screen") { DataConfirmScreen(navController) }
-                        composable("registration_screen") { RegistrationScreen(navController) }
-                        composable("question_screen") { QuestionScreen(navController) }
-                        composable("confirmation_screen") { ConfirmationScreen(navController) }
+                        composable("registration_screen") { RegistrationScreen(navController, viewModel) }
+                        composable("question_screen") { QuestionScreen(navController, viewModel) }
+                        composable("confirmation_screen") { ConfirmationScreen(navController, viewModel) }
                         composable("cadastrado_screen") { CadastradoScreen(navController) }
                     }
                 }
             }
         }
     }
+}
+
+class DataViewModel : ViewModel() {
+    var name = MutableLiveData<String>()
+    var email = MutableLiveData<String>()
+    var dateOfBirth = MutableLiveData<String>()
+    var question1 = MutableLiveData<String>()
+    var question2 = MutableLiveData<String>()
+    var question3 = MutableLiveData<String>()
 }
 
 @Composable
@@ -98,97 +113,140 @@ fun DataConfirmScreen(navController: NavController) {
 }
 
 @Composable
-fun RegistrationScreen(navController: NavController) {
+fun RegistrationScreen(navController: NavController, viewModel: DataViewModel) {
 
     val name = rememberSaveable { mutableStateOf("") }
     val email = rememberSaveable { mutableStateOf("") }
     val dateOfBirth = rememberSaveable { mutableStateOf("") }
     val password = rememberSaveable { mutableStateOf("") }
 
+    val isNameError = remember { mutableStateOf(false) }
+    val isEmailError = remember { mutableStateOf(false) }
+    val isDateOfBirthError = remember { mutableStateOf(false) }
+    val isPasswordError = remember { mutableStateOf(false) }
+
     IconButton(onClick = { navController.popBackStack() }) {
         Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Voltar")
     }
 
     Column(
-    modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp),
-    horizontalAlignment = Alignment.CenterHorizontally
-) {
-    Text(
-        text = "Cadastro",
-        fontSize = 24.sp,
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(bottom = 16.dp)
-    )
-
-    OutlinedTextField(
-        value = name.value,
-        onValueChange = { newName -> name.value = newName },
-        label = { Text("Nome") },
-        modifier = Modifier.fillMaxWidth()
-    )
-
-    Spacer(modifier = Modifier.height(8.dp))
-
-    OutlinedTextField(
-        value = email.value,
-        onValueChange = { newEmail -> email.value = newEmail },
-        label = { Text("Email") },
-        modifier = Modifier.fillMaxWidth()
-    )
-
-    Spacer(modifier = Modifier.height(8.dp))
-
-    OutlinedTextField(
-        value = dateOfBirth.value,
-        onValueChange = { newDateOfBirth -> dateOfBirth.value = newDateOfBirth },
-        label = { Text("Data de Nascimento") },
-        modifier = Modifier.fillMaxWidth()
-    )
-
-    Spacer(modifier = Modifier.height(8.dp))
-
-    OutlinedTextField(
-        value = password.value,
-        onValueChange = { newPassword -> password.value = newPassword },
-        label = { Text("Senha") },
-        visualTransformation = PasswordVisualTransformation(),
-        modifier = Modifier.fillMaxWidth()
-    )
-
-    Spacer(modifier = Modifier.height(32.dp))
-
-    Button(
-        onClick = { navController.navigate("question_screen?name=${name.value}&email=${email.value}&dateOfBirth=${dateOfBirth.value}")
-            println("Name Question: ${name.value}")
-            println("Email Question: ${email.value}")
-            println("Data de Nascimento Question: ${dateOfBirth.value}")},
         modifier = Modifier
-            .fillMaxWidth()
-            .height(48.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Blue,
-            contentColor = Color.White
-        )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Confirmar")
-    }
+        Text(
+            text = "Cadastro",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
 
-    Spacer(modifier = Modifier.height(16.dp))
-}
+        OutlinedTextField(
+            value = name.value,
+            onValueChange = { newName -> name.value = newName },
+            label = { Text("Nome") },
+            modifier = Modifier.fillMaxWidth(),
+            isError = isNameError.value
+        )
+        if (isNameError.value) {
+            Text(text = "Nome é obrigatório", color = Color.Red)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = email.value,
+            onValueChange = { newEmail -> email.value = newEmail },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth(),
+            isError = isEmailError.value
+        )
+        if (isEmailError.value) {
+            Text(text = "Email é obrigatório", color = Color.Red)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = dateOfBirth.value,
+            onValueChange = { newDateOfBirth -> dateOfBirth.value = newDateOfBirth },
+            label = { Text("Data de Nascimento") },
+            modifier = Modifier.fillMaxWidth(),
+            isError = isDateOfBirthError.value
+        )
+        if (isDateOfBirthError.value) {
+            Text(text = "Data de Nascimento é obrigatório", color = Color.Red)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password.value,
+            onValueChange = { newPassword -> password.value = newPassword },
+            label = { Text("Senha") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(),
+            isError = isPasswordError.value
+        )
+        if (isPasswordError.value) {
+            Text(text = "Senha é obrigatório", color = Color.Red)
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        fun validateForm(
+            name: MutableState<String>,
+            email: MutableState<String>,
+            dateOfBirth: MutableState<String>,
+            password: MutableState<String>
+        ): Boolean {
+            isNameError.value = name.value.isBlank()
+            isEmailError.value = email.value.isBlank()
+            isDateOfBirthError.value = dateOfBirth.value.isBlank()
+            isPasswordError.value = password.value.isBlank()
+
+            return !isNameError.value && !isEmailError.value && !isDateOfBirthError.value && !isPasswordError.value
+        }
+
+
+        Button(
+            onClick = {
+                val isValid = validateForm(name, email, dateOfBirth, password)
+
+                if (isValid) {
+                    viewModel.name.value = name.value
+                    viewModel.email.value = email.value
+                    viewModel.dateOfBirth.value = dateOfBirth.value
+                    navController.navigate("question_screen")
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue,
+                contentColor = Color.White
+            )
+        ) {
+            Text("Confirmar")
+        }
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+    }
 }
 
 @Composable
-fun QuestionScreen(navController: NavController) {
+fun QuestionScreen(navController: NavController, viewModel: DataViewModel) {
 
-    val name = rememberSaveable { mutableStateOf(navController.currentBackStackEntry?.arguments?.getString("name") ?: "") }
-    val email = rememberSaveable { mutableStateOf(navController.currentBackStackEntry?.arguments?.getString("email") ?: "") }
-    val dateOfBirth = rememberSaveable { mutableStateOf(navController.currentBackStackEntry?.arguments?.getString("dateOfBirth") ?: "") }
+    val question1 = rememberSaveable { mutableStateOf("") }
+    val question2 = rememberSaveable { mutableStateOf("") }
+    val question3 = rememberSaveable { mutableStateOf("") }
 
-    val question1 = remember { mutableStateOf("") }
-    val question2 = remember { mutableStateOf("") }
-    val question3 = remember { mutableStateOf("") }
+    val isQuestion1Error = remember { mutableStateOf(false) }
+    val isQuestion2Error = remember { mutableStateOf(false) }
+    val isQuestion3Error = remember { mutableStateOf(false) }
 
     IconButton(onClick = { navController.popBackStack() }) {
         Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Voltar")
@@ -215,10 +273,14 @@ fun QuestionScreen(navController: NavController) {
 
         TextField(
             value = question1.value,
-            onValueChange = { question1.value = it  },
+            onValueChange = { question1.value = it },
             label = { Text("Ex: Python, Java, JavaScript") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            isError = isQuestion1Error.value
         )
+        if (isQuestion1Error.value) {
+            Text(text = "Questão obrigatória!", color = Color.Red)
+        }
 
         Spacer(modifier = Modifier.height(8.dp))
 
@@ -233,8 +295,12 @@ fun QuestionScreen(navController: NavController) {
             value = question2.value,
             onValueChange = { question2.value = it },
             label = { Text("0-10") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            isError = isQuestion2Error.value
         )
+        if (isQuestion2Error.value) {
+            Text(text = "Questão obrigatória!", color = Color.Red)
+        }
 
         Spacer(modifier = Modifier.height(8.dp))
 
@@ -249,19 +315,38 @@ fun QuestionScreen(navController: NavController) {
             value = question3.value,
             onValueChange = { question3.value = it },
             label = { Text("Escreva aqui") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            isError = isQuestion3Error.value
         )
+        if (isQuestion3Error.value) {
+            Text(text = "Questão obrigatória!", color = Color.Red)
+        }
 
         Spacer(modifier = Modifier.height(32.dp))
 
+        fun validateForm(
+            question1: MutableState<String>,
+            question2: MutableState<String>,
+            question3: MutableState<String>,
+        ): Boolean {
+            isQuestion1Error.value = question1.value.isBlank()
+            isQuestion2Error.value = question2.value.isBlank()
+            isQuestion3Error.value = question3.value.isBlank()
+
+            return !isQuestion1Error.value && !isQuestion2Error.value && !isQuestion3Error.value
+        }
+
         Button(
-            onClick = { navController.navigate("confirmation_screen?name=$name&email=$email&dateOfBirth=$dateOfBirth&question1=${question1.value}&question2=${question2.value}&question3=${question3.value}")
-                println("Name Question: ${name}")
-                println("Email Question: ${email}")
-                println("Data de Nascimento Question: ${dateOfBirth}")
-                println("Queston1 Question: ${question1.value}")
-                println("Question2 Question: ${question2.value}")
-                println("Question3 Question: ${question3.value}")},
+            onClick = {
+                val isValid = validateForm(question1, question2, question3)
+
+                if (isValid) {
+                    viewModel.question1.value = question1.value
+                    viewModel.question2.value = question2.value
+                    viewModel.question3.value = question3.value
+                    navController.navigate("confirmation_screen")
+                }
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .height(48.dp),
@@ -272,21 +357,18 @@ fun QuestionScreen(navController: NavController) {
         ) {
             Text("Confirmar")
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
 @Composable
-fun ConfirmationScreen(navController: NavController) {
-    val backStackEntry = navController.previousBackStackEntry?.savedStateHandle
+fun ConfirmationScreen(navController: NavController, viewModel: DataViewModel) {
 
-    val name = backStackEntry?.get<String>("name") ?: "Nome não encontrado"
-    val email = backStackEntry?.get<String>("email") ?: "Email não encontrado"
-    val dateOfBirth = backStackEntry?.get<String>("dateOfBirth") ?: "Data de Nascimento não encontrada."
-    val question1 = backStackEntry?.get<String>("question1") ?: ""
-    val question2 = backStackEntry?.get<String>("question2") ?: ""
-    val question3 = backStackEntry?.get<String>("question3") ?: ""
+    val name = viewModel.name.value ?: "Nome não encontrado"
+    val email = viewModel.email.value ?: "Email não encontrado"
+    val dateOfBirth = viewModel.dateOfBirth.value ?: "Nome não encontrado"
+    val question1 = viewModel.question1.value ?: "Email não encontrado"
+    val question2 = viewModel.question2.value ?: "Nome não encontrado"
+    val question3 = viewModel.question3.value ?: "Email não encontrado"
 
     IconButton(onClick = { navController.popBackStack() }) {
         Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Voltar")
@@ -372,6 +454,8 @@ fun ConfirmationScreen(navController: NavController) {
         }
     }
 }
+
+
 
 @Composable
 fun CadastradoScreen(navController: NavController) {
